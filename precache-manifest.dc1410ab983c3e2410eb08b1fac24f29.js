d24945d5568440e461cba678ded97fd51e9d0b89
tadataku-lab <tadataku.lab@gmail.com>
2019-11-01 14:46:22 +0900

    Updates

---
+++ precache-manifest.dc1410ab983c3e2410eb08b1fac24f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b242f1a0bc8040df9cc4c955dd95894f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a6379b6296965705158",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c839c661c2ff94c76ccf",
+    "url": "/static/js/2.2c03015e.chunk.js"
+  },
+  {
+    "revision": "7a6379b6296965705158",
+    "url": "/static/js/main.19f45c47.chunk.js"
+  },
+  {
+    "revision": "2fa8a2aba81861967f3b",
+    "url": "/static/js/runtime-main.00acfda8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
